e9c6bf0264cf6a3172b7aad791bf8361921f6a0f
mehabubi-meh <mehabubimeh0708@gmail.com>
2025-07-01 12:19:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+//your code here
+const inputBox = document.getElementById("evaluatedText");
+const counter = document.getElementById("letterCount");
+
+inputBox.addEventListener("input", () => {
+  counter.innerText = inputBox.value.length;
+});
